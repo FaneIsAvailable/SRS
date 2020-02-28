6c556f09107c51e5c2e57f195d66a274c9bba823
GhermanB <GhermanB@GhermanB-PC>
2020-02-28 14:20:20 +0200

    --- automatic rebase to remote; old local SHA: e8c6ec8a0fdc7dac1587dda515ae3ed9195d2f40 ---

--- src/application/PushDa.java
+++ src/application/PushDa.java
@@ -4,7 +4,13 @@ package application;
 import javax.inject.Inject;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.controllerModel.Controller;
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.World;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
 
 /**
  * Implementation of a robot application.
@@ -27,15 +33,91 @@ import com.kuka.roboticsAPI.deviceModel.LBR;
 public class PushDa extends RoboticsAPIApplication {
 	@Inject
 	private LBR lBR_iiwa_7_R800_1;
+	private Controller myController;
+	private LBR myLBR;
+	private ObjectFrame myWorld;
+	private Tool acu;
+	private ObjectFrame tcpAc;
 
 	@Override
 	public void initialize() {
 		// initialize your application here
+		myController = getController("KUKA_Sunrise_Cabinet_1");
+		myLBR = (LBR) getDevice(myController,
+				"LBR_iiwa_7_R800_1");
+		myWorld = World.Current.getRootFrame();
+		acu=getApplicationData().createFromTemplate("DaNeedle");
+		tcpAc=acu.getFrame("DaTCP");
+		acu.attachTo(myLBR.getFlange());
+
+
 	}
 
 	@Override
 	public void run() {
 		// your application execution starts here
-		lBR_iiwa_7_R800_1.move(ptpHome());
+		myLBR.move(ptpHome());
+		int result=0;
+		getLogger().info("Starting BlueGear");
+		do
+		{
+		result=getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, "Please select", 
+					"Exit", "Move Preinsertion","Move insertion", "Move target", "Extract");
+
+		switch (result) {
+		case 0:
+			getLogger().info("Exiting");
+			break;
+		case 1:
+			getLogger().info("Move pre_insert");
+			movepreins();
+			break;
+		case 2:
+			getLogger().info("Move insert");
+			moveins();
+			break;
+		case 3:
+			getLogger().info("move target");
+			movetarg();
+			break;
+		case 4:
+			getLogger().info("extract");
+			extract();
+			break;
+		default:
+			getLogger().info("Nothing Selected");
+			break;
+		}
+
+		}while (0!=result);
+		
+		
+	}
+
+	private void extract() {
+		// TODO Auto-generated method stub
+		getLogger().info("ins/extract");
+		acu.move(lin(getApplicationData().getFrame("/Insertion")));
+	}
+
+	private void movetarg() {
+		// TODO Auto-generated method stub
+		getLogger().info("target");
+		acu.move(lin(getApplicationData().getFrame("/Target")));
+		
+	}
+
+	private void moveins() {
+		// TODO Auto-generated method stub
+		getLogger().info("ins");
+		acu.move(lin(getApplicationData().getFrame("/Insertion")));
+		
+	}
+
+	private void movepreins() {
+		// TODO Auto-generated method stub
+		getLogger().info("preins");
+		acu.move(ptp(getApplicationData().getFrame("/preinsertion")));
+		
 	}
 }
