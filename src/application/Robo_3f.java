f4df0e42f8678be0eae48523ff0ce5b989e52587
GhermanB <GhermanB@GhermanB-PC>
2020-05-21 18:13:40 +0300

    --- automatic rebase to remote; old local SHA: 3a17123ed5c2ddcea84d280b0f4d1059919afdfc ---

--- src/application/Robo_3f.java
+++ src/application/Robo_3f.java
@@ -39,12 +39,12 @@ public class Robo_3f extends Robotiq3FIOGroup {
 	//************************************************** 
 	public void close_full(){
 		
-		this.setSpeed_A(10);
-		this.setSpeed_B(10);
-		this.setSpeed_C(10);
-		this.setPos_req_A(200);
-		this.setPos_req_B(200);
-		this.setPos_req_C(200);
+		this.setSpeed_A(100);
+		this.setSpeed_B(100);
+		this.setSpeed_C(100);
+		this.setPos_req_A(255);
+		this.setPos_req_B(255);
+		this.setPos_req_C(255);
 		//this.setPos_req_scissor(250);
 		//this.setAct_req(9);
 	
@@ -52,9 +52,9 @@ public class Robo_3f extends Robotiq3FIOGroup {
 	public void open_full()
 	{
 		
-		this.setSpeed_A(10);
-		this.setSpeed_B(10);
-		this.setSpeed_C(10);
+		this.setSpeed_A(100);
+		this.setSpeed_B(100);
+		this.setSpeed_C(100);
 		this.setPos_req_A(0);
 		this.setPos_req_B(0);
 		this.setPos_req_C(0);
