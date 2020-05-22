4769955b941bcc1c69c717be1454317fe04f0be5
GhermanB <GhermanB@GhermanB-PC>
2020-05-22 08:15:39 +0300

    --- automatic rebase to remote; old local SHA: 76400946ccd564955edf852fa9a26e66a50bca87 ---

--- src/application/RunAway.java
+++ src/application/RunAway.java
@@ -86,9 +86,9 @@ public class RunAway extends RoboticsAPIApplication {
 		};
 		//acu.attachTo(lBR_iiwa_7_R800_1.getFlange());
 		
-		ForceCondition forta_Z=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.Z, 200);
-		ForceCondition forta_Y=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.Y, 200);
-		ForceCondition forta_X=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.X, 200);
+		ForceCondition forta_Z=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.Z, 2000);
+		ForceCondition forta_Y=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.Y, 2000);
+		ForceCondition forta_X=ForceCondition.createNormalForceCondition(tcpAc, CoordinateAxis.X, 2000);
 		tcpAc.move(linRel(0,0,10).triggerWhen(forta_Z, getPosaction));
 	
 	
