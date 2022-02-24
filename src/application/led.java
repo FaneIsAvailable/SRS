cf24981b56061ad62a7f1e22838ba4027c8dfe26
Calin <Calin@DESKTOP-8RG1AGQ>
2022-02-24 10:45:57 +0200

    --- automatic rebase to remote; old local SHA: ddb333df8f1a3df93acf639189c26c7c02b4312a ---

---
+++ src/application/led.java
@@ -0,0 +1,50 @@
+package application;
+
+
+import javax.inject.Inject;
+
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class led extends RoboticsAPIApplication {
+	@Inject
+	private LBR myLbr;
+	private Controller myController;
+	private MediaFlangeIOGroup led;
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+		myController = getController("KUKA_Sunrise_Cabinet_1");
+		myLbr.getContext().getDeviceFromType(LBR.class);
+		led=new MediaFlangeIOGroup(myController);
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		myLbr.move(ptpHome());
+	}
+}
